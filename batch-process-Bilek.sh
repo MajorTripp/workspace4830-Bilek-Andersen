8d8aefddc797402e4fb92f3032487f93aac36539
MajorTripp <jeradtripp@gmail.com>
2023-09-28 20:07:30 -0700

    Actual Thing

--- batch-process-Bilek.sh
+++ batch-process-Bilek.sh
@@ -1,1 +1,10 @@
-test
+#! /bin/sh
+for f in "$1"/*; do
+  if [ -f "$f" ]; then
+    WORDS="$(wc -w "${f}"| cut -d ' ' -f1)"
+    SIZE="$(du -sh "${f}" | cut -f1)"
+    echo "Processing $f file..."
+    echo "$SIZE"
+    echo "$WORDS"
+  fi
+done
