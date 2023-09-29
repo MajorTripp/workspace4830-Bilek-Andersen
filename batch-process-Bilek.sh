1857cfeab02b6d411802e69d9d14d27e095429e2
MajorTripp <jeradtripp@gmail.com>
2023-09-28 21:55:00 -0700

    Added some more text

--- batch-process-Bilek.sh
+++ batch-process-Bilek.sh
@@ -4,7 +4,7 @@ for f in "$1"/*; do
     WORDS="$(wc -w "${f}"| cut -d ' ' -f1)"
     SIZE="$(du -sh "${f}" | cut -f1)"
     echo "Processing $f file..."
-    echo "$SIZE"
-    echo "$WORDS"
+    echo "File Size: $SIZE"
+    echo "Word Count: $WORDS"
   fi
 done
